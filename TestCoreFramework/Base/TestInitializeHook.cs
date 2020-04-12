aba46a96d7991200893ae5f3de65e6641cd959ac
Peter Lukic <peter.lukic@gmail.com>
2020-04-12 11:15:32 +0200

    Add folder WebDrivers

--- TestCoreFramework/Base/TestInitializeHook.cs
+++ TestCoreFramework/Base/TestInitializeHook.cs
@@ -1,11 +1,14 @@
-﻿using TestCoreFramework.Config;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using TestCoreFramework.Config;
 using TestCoreFramework.Helpers;
 using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Chrome;
 using TechTalk.SpecFlow;
-using OpenQA.Selenium.Remote;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
 
 namespace TestCoreFramework.Base
 { public class TestInitializeHook : Steps
@@ -25,22 +28,38 @@ namespace TestCoreFramework.Base
             OpenBrowser(GetBrowserOption(Settings.BrowserType));
             //LogHelpers.Write("Initialized framework");
         }
+
+        private static string GetDriversDirectoryPath()
+        {
+            var assemblyUri = new Uri(Assembly.GetExecutingAssembly().CodeBase).AbsolutePath;
+            return Path.Combine(Path.GetDirectoryName(assemblyUri), "WebDrivers");
+        }
         private void OpenBrowser(DriverOptions driverOptions)
         {
             switch (driverOptions)
             {
                 case InternetExplorerOptions internetExplorerOptions:
                     //ToDo: Set the Desired capabilities
-                    driverOptions = new InternetExplorerOptions();
+                    var options = new InternetExplorerOptions();
+                    options.IntroduceInstabilityByIgnoringProtectedModeSettings = true;
+                    _parallelConfig.Driver = new InternetExplorerDriver(GetDriversDirectoryPath(), options);
+                    _parallelConfig.Driver.Manage().Window.Maximize();
                     break;
                 case FirefoxOptions firefoxOptions:
-                    firefoxOptions.AddAdditionalCapability(CapabilityType.BrowserName, "firefox");
-                    firefoxOptions.AddAdditionalCapability(CapabilityType.Platform, new Platform(PlatformType.Windows));
-                    firefoxOptions.BrowserExecutableLocation = @"C:\Program Files (x86)\Mozilla Firefox\firefox.exe";
+                    var ffOptions = new FirefoxOptions();
+                    ffOptions.AddArgument("--no-sandbox");
+                    ffOptions.SetPreference("capability.policy.default.Window.frameElement.get", "allAccess");
+                    _parallelConfig.Driver = new FirefoxDriver(GetDriversDirectoryPath(), ffOptions, TimeSpan.FromMinutes(3));
+                    _parallelConfig.Driver.Manage().Window.Maximize();
                     break;
                 case ChromeOptions chromeOptions:
-                    chromeOptions.AddAdditionalCapability(CapabilityType.EnableProfiling, true, true);
-                    _parallelConfig.Driver = new ChromeDriver();
+                    chromeOptions.AddArgument("start-maximized");
+                    chromeOptions.AddArgument("--no-sandbox");
+                    _parallelConfig.Driver = new ChromeDriver(GetDriversDirectoryPath(), chromeOptions, TimeSpan.FromMinutes(3));
+                    break;
+                case EdgeOptions edgeOptions:
+                    edgeOptions.AddExtensionPath(GetDriversDirectoryPath());
+                    _parallelConfig.Driver = new EdgeDriver(GetDriversDirectoryPath());
                     _parallelConfig.Driver.Manage().Window.Maximize();
                     break;
             }
